412436896001a372bd45b54228b7820872b2f165
wuzhixuan <jackwu@java.com>
2022-03-30 00:51:09 +0800

    feat: oauth认证服务器 + 资源服务器搭建 jwt改造

--- oauth-server/src/main/java/com/wuzx/config/OauthServerConfiger.java
+++ oauth-server/src/main/java/com/wuzx/config/OauthServerConfiger.java
@@ -1,18 +1,26 @@
 package com.wuzx.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -43,22 +51,45 @@ public class OauthServerConfiger extends AuthorizationServerConfigurerAdapter {
                 .checkTokenAccess("permitAll()");
     }
 
+//    /**
+//     * 从内存客户端详情配置，
+//     *
+//     * @param clients
+//     * @throws Exception
+//     */
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        super.configure(clients);
+//        clients.inMemory() // 客户端信息存储在什么地方，可以在内存中，可以在数据库里
+//                .withClient("client_lagou") // 添加一个client配置,指定id
+//                .secret("abcxyz") // 指定客户端的密码/安全码
+//                .resourceIds("autodeliver") // 指定客户端 所能访问资源id清单，此处的资源id是需要在具体的资源服务器上也配置一样
+//                .authorizedGrantTypes("password", "refresh_token") // 认证类型/令牌颁发模式，可以配置多个在这里，但是不一定 都用，具体使用哪种方式颁发token，需要客户端调用的时候传递参数指定
+//                .scopes("all"); // 客户端的权限范围，此处配置为all全部即可
+//
+//    }
+
+
     /**
-     * 客户端详情配置，
-     *
+     * 从数据库
      * @param clients
      * @throws Exception
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         super.configure(clients);
-        clients.inMemory() // 客户端信息存储在什么地方，可以在内存中，可以在数据库里
-                .withClient("client_lagou") // 添加一个client配置,指定id
-                .secret("abcxyz") // 指定客户端的密码/安全码
-                .resourceIds("autodeliver") // 指定客户端 所能访问资源id清单，此处的资源id是需要在具体的资源服务器上也配置一样
-                .authorizedGrantTypes("password", "refresh_token") // 认证类型/令牌颁发模式，可以配置多个在这里，但是不一定 都用，具体使用哪种方式颁发token，需要客户端调用的时候传递参数指定
-                .scopes("all"); // 客户端的权限范围，此处配置为all全部即可
 
+        clients.withClientDetails(clientDetailsService());
+    }
+
+
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    public ClientDetailsService clientDetailsService() {
+        return new JdbcClientDetailsService(dataSource);
     }
 
     /**
@@ -82,16 +113,34 @@ public class OauthServerConfiger extends AuthorizationServerConfigurerAdapter {
      * @return
      */
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+//        return new InMemoryTokenStore();
+        return new JwtTokenStore(jwtAccessTokenConverter());
     }
+
+
+    private String sign_key = "wuzx";
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(sign_key); // 签名秘钥
+        converter.setVerifier(new MacSigner(sign_key)); // 验证时使用的秘钥，和签名秘钥保持一致
+
+        return converter;
+    }
+
 
     public AuthorizationServerTokenServices authorizationServerTokenServices() {
         // 使用默认实现
         DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
         defaultTokenServices.setSupportRefreshToken(true); // 是 否开启令牌刷新
         defaultTokenServices.setTokenStore(tokenStore());
-// 设置令牌有效时间(一般设置为2个小时) defaultTokenServices.setAccessTokenValiditySeconds(20);
-// access_token就是我们请求资源需要携带的令牌 // 设置刷新令牌的有效时间
+
+
+        // 针对jwt令牌添加
+        defaultTokenServices.setTokenEnhancer(jwtAccessTokenConverter());
+
+        // 设置令牌有效时间(一般设置为2个小时)
+        defaultTokenServices.setAccessTokenValiditySeconds(2000); // access_token就是我们请求资源需要携带的令牌
+        // 设置刷新令牌的有效时间
         defaultTokenServices.setRefreshTokenValiditySeconds(259200); // 3天
         return defaultTokenServices;
     }
--- service-producer/src/main/java/com/wuzx/config/ResourceServerConfiger.java
+++ service-producer/src/main/java/com/wuzx/config/ResourceServerConfiger.java
@@ -4,10 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 
 /**
@@ -23,27 +27,63 @@ public class ResourceServerConfiger extends ResourceServerConfigurerAdapter {
     }
 
     /**
-     * 该方法用于定义资源服务器向远程认证服务器发起请求，进行token校验等事宜
-     *
+     * jwt令牌改造
      * @param resources
      * @throws Exception
      */
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        // 设置当前资源服务的资源id
-        resources.resourceId("autodeliver");
-
-        // 定义token服务对象(token校验就应该靠token服务对象)
-        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
-
-        // 校验端点/接口设置
-        remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:9999/oauth/check_token");
-
-        remoteTokenServices.setClientId("client_lagou");
-        remoteTokenServices.setClientSecret("abcxyz");
-        // 别忘了这一步
-        resources.tokenServices(remoteTokenServices);
+        resources.resourceId("autodeliver")
+                .tokenStore(tokenStore())
+                .stateless(true) // 无状态设置
+        ;
     }
+
+    /**
+     * 该方法用于创建tokenStore对象(令牌存储对象)
+     * @return
+     */
+    public TokenStore tokenStore(){
+//        return new InMemoryTokenStore();
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+
+    private String sign_key = "wuzx";
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(sign_key); // 签名秘钥
+        converter.setVerifier(new MacSigner(sign_key)); // 验证时使用的秘钥，和签名秘钥保持一致
+
+        return converter;
+    }
+
+
+
+    /**
+     * 该方法用于定义资源服务器向远程认证服务器发起请求，进行token校验等事宜
+     *
+     * @param resources
+     * @throws Exception
+     */
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        // 设置当前资源服务的资源id
+//        resources.resourceId("autodeliver");
+//
+//        // 定义token服务对象(token校验就应该靠token服务对象)
+//        RemoteTokenServices remoteTokenServices = new RemoteTokenServices();
+//
+//        // 校验端点/接口设置
+//        remoteTokenServices.setCheckTokenEndpointUrl("http://localhost:9999/oauth/check_token");
+//
+//        remoteTokenServices.setClientId("client_lagou");
+//        remoteTokenServices.setClientSecret("abcxyz");
+//        // 别忘了这一步
+//        resources.tokenServices(remoteTokenServices);
+//    }
+
+
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
